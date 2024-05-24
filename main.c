5c2d217f1b20c82a6899e9d2441fd467ba86ec05
Chan Heo <chan0822kr@gmail.com>
2024-05-24 17:07:20 +0900

    부산행 2탄 - 네 번째 커밋
    내용 :
    1. 어그로 속성 추가,
    
    2. Sleep 말고 마동석 움직임 입력으로 턴 전환
    
    3. aggro 음수 되거나 5보다 커지는 문제 해결
    
    4.시민 어그로도 추가
    
    다음에 할 것:
    
    1. 마동석 이동 입력 후에 열차 내부 표현
    
    2. 마동석 이동 결과 출력
    
    3. 마동석과 시민 어그로 증감 표현
    
    ---> <이동> 페이즈 다 끝낼 것.

--- main.c
+++ main.c
@@ -161,6 +161,10 @@ int main() {
 	int zombie = len - 3;
 	int civil = len - 6;
 	int madong = len - 2;
+
+	int aggro_civil = 1; // 시민 어그로
+	int aggro_madon = 1; // 마동석 어그로
+
 	
 	int scene = 1; // 짝수 횟수에 좀비가 멈추기 위한 장면 수 
 	while (1) {
@@ -170,10 +174,18 @@ int main() {
 		int r = rand() % 100 + 1;  // 1 ~ 100 까지의 난수 생성
 		if (r > p) { // 난수가 P 보다 크면 이동, 즉 (1-p)의 확률로 이동
 			civil--;
+			aggro_civil++;
+			if (aggro_civil > 5) {
+				aggro_civil = 5;
+			}
 		}
 		else {
 			if (scene % 2 != 0) {
-				zombie--;
+				zombie--;  // 어그로가 높은 쪽을 선택해서 가려면 이 부분을 수정해야 한다. ------------------------------------------ 아마 이중 조건문으로 작성해야 될 듯 
+				aggro_civil--;
+				if (aggro_civil < 0) {
+					aggro_civil = 0;
+				}
 			}
 		}
 
@@ -183,23 +195,23 @@ int main() {
 		printf("\n");
 
 		// 시민과 좀비의 이동 결과
-		if (scene % 2 == 0) {
+		if (scene % 2 == 0) { // 짝수 번째 장면 (좀비가 못움직이는 장면)
 			if (r > p) {
-				printf("Citizen : %d -> %d\n", civil + 1, civil );
+				printf("Citizen : %d -> %d (aggro : %d)\n", civil + 1, civil, aggro_civil);
 				printf("Zombie : Stay %d (can not move)\n", zombie );
 			}
 			else {
-				printf("Citizen : Stay %d\n", civil );
+				printf("Citizen : Stay %d (aggro : %d)\n", civil, aggro_civil);
 				printf("Zombie : Stay %d (can not move)\n", zombie );
 			}
 		}
-		else {
+		else { // 홀 수 번째 장면 (좀비가 움직일 수 없는 장면)
 			if (r > p) {
-				printf("Citizen : %d -> %d\n", civil + 1, civil );
+				printf("Citizen : %d -> %d (aggro : %d)\n", civil + 1, civil, aggro_civil);
 				printf("Zombie : Stay %d \n", zombie );
 			}
 			else {
-				printf("Citizen : Stay %d\n", civil );
+				printf("Citizen : Stay %d (aggro : %d)\n", civil,aggro_civil);
 				printf("Zombie : %d -> %d\n", zombie + 1, zombie );
 			}
 		}
@@ -207,15 +219,16 @@ int main() {
 		printf("\n");
 
 		// 마동석 이동 입력 및 결과
-		while (!(madong_move == 0 || madong_move == 1)) {
+		while (!(madong_move == MOVE_LEFT || madong_move == MOVE_STAY)) {
 			printf("madongseock move(0 : stay, 1 : move) >> ");
 			scanf_s("%d", &madong_move);
 		}
-		
-		if (madong_move == 1) {
+		printf("\n");
+
+		if (madong_move == MOVE_LEFT) {
 			printf("madong move");
 		}
-		else if (madong_move == 0) {
+		else if (madong_move == MOVE_STAY) {
 			printf("modong doesnt move");
 		}
 
@@ -231,7 +244,6 @@ int main() {
 			printf("\nGAME OVER!\n");
 			printf("Citizen(s) has(have) been attacked by a zombie\n");
 			break;
-
 		}
 
 		printf("\n\n");
